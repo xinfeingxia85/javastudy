28c479522165d8b6842ac861056cbb9fea1b14da
fangkaiming <fangkaiming@126.com>
2017-07-07 11:41:58 +0800

    体验一下

--- src/com/study/Demo1.java
+++ src/com/study/Demo1.java
@@ -3,8 +3,10 @@ package com.study;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("ddd");
-		System.out.println("hahahah");
+		String string = "我是你的b印";
+		for (int i = 0; i < string.length(); i++) {
+			System.out.println((int)string.charAt(i));
+			System.out.println((char)21360);
+		}
 	}
-
 }
